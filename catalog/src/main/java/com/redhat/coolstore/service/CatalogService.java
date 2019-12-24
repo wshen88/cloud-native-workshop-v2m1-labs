19d3b555046ae4f45f5a06199767423646a916c2
User0 Developer <user0@no-reply.com>
2019-12-24 14:21:19 +0000

    Step 6 of section 6

---
+++ catalog/src/main/java/com/redhat/coolstore/service/CatalogService.java
@@ -0,0 +1,35 @@
+package com.redhat.coolstore.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+//import com.redhat.coolstore.client.InventoryClient;
+import com.redhat.coolstore.model.Product;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CatalogService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    //TODO: Autowire Inventory Client
+
+    public Product read(String id) {
+        Product product = repository.findById(id);
+        //TODO: Update the quantity for the product by calling the Inventory service
+        return product;
+    }
+
+    public List<Product> readAll() {
+        List<Product> productList = repository.readAll();
+        //TODO: Update the quantity for the products by calling the Inventory service
+        return productList;
+    }
+
+}
